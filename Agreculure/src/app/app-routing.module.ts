173400a6d60a3f10dec7e89a966c7b1d51ea87d8
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-16 22:13:46 +0500

    components

--- Agreculure/src/app/app-routing.module.ts
+++ Agreculure/src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { ShopsingleComponent } from './pages/shopsingle/shopsingle.component';
+import { ServiceComponent } from './pages/service/service.component';
+
+
 
 const routes: Routes = [
-  { path: 'main', component: MainComponent}
+  { path: 'main', component: MainComponent},
+  {path:'shop',component:ShopComponent},
+  {path:'shopsingle',component:ShopsingleComponent},
+  {path:'service',component:ServiceComponent}
+
+  
 ];
 
 @NgModule({
