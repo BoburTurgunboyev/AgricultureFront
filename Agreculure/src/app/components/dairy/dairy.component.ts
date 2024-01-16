17afc87cd9d8ba127e27d5bfd27f86b716d4802d
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-16 22:15:21 +0500

    components

---
+++ Agreculure/src/app/components/dairy/dairy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dairy',
+  templateUrl: './dairy.component.html',
+  styleUrl: './dairy.component.scss'
+})
+export class DairyComponent {
+
+}
