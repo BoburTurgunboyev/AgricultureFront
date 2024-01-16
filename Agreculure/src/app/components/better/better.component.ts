2c6be7bc72c8be141d76cbe0f56ec10618586149
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-16 22:14:16 +0500

    components

---
+++ Agreculure/src/app/components/better/better.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-better',
+  templateUrl: './better.component.html',
+  styleUrl: './better.component.scss'
+})
+export class BetterComponent {
+
+}
