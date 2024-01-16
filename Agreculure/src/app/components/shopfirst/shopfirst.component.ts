58e3b2c69ff1bb46a86d27b109eeee8bde72b43f
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-16 22:17:12 +0500

    components

---
+++ Agreculure/src/app/components/shopfirst/shopfirst.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopfirst',
+  templateUrl: './shopfirst.component.html',
+  styleUrl: './shopfirst.component.scss'
+})
+export class ShopfirstComponent {
+
+}
