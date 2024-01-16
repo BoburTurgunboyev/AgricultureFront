582ee1b3539d3d720aeaccf6b8d01ec899dc8c12
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-16 22:16:57 +0500

    components

---
+++ Agreculure/src/app/components/servicefirst/servicefirst.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicefirst',
+  templateUrl: './servicefirst.component.html',
+  styleUrl: './servicefirst.component.scss'
+})
+export class ServicefirstComponent {
+
+}
