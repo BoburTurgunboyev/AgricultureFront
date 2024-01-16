510a47f813abe35bbedcf2f269d4b6544a8f5ea7
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-16 22:15:54 +0500

    components

---
+++ Agreculure/src/app/components/offer/offer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer',
+  templateUrl: './offer.component.html',
+  styleUrl: './offer.component.scss'
+})
+export class OfferComponent {
+
+}
