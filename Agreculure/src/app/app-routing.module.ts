4194d1d3b919776c23975590773d26823f5ca4ac
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-17 23:00:51 +0500

    components

--- Agreculure/src/app/app-routing.module.ts
+++ Agreculure/src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { MainComponent } from './pages/main/main.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { ShopsingleComponent } from './pages/shopsingle/shopsingle.component';
 import { ServiceComponent } from './pages/service/service.component';
+import { ServicesingleComponent } from './pages/servicesingle/servicesingle.component';
+import path from 'path';
+import { AboutComponent } from './pages/about/about.component';
+import { NewsComponent } from './pages/news/news.component';
 
 
 
@@ -11,7 +15,10 @@ const routes: Routes = [
   { path: 'main', component: MainComponent},
   {path:'shop',component:ShopComponent},
   {path:'shopsingle',component:ShopsingleComponent},
-  {path:'service',component:ServiceComponent}
+  {path:'service',component:ServiceComponent},
+  {path:'servicesingle',component:ServicesingleComponent},
+  {path:'about',component:AboutComponent},
+  {path:'news',component:NewsComponent}
 
   
 ];
