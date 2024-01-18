2c29de28afc3f43239666800213f68cf636b9f0b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-18 20:01:17 +0500

    components

---
+++ Agreculure/src/app/components/vitamin/vitamin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vitamin',
+  templateUrl: './vitamin.component.html',
+  styleUrl: './vitamin.component.scss'
+})
+export class VitaminComponent {
+
+}
