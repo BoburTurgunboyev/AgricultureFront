82e75a47251e9133b4e847b7a4d504c4fe721522
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-18 20:00:29 +0500

    components

---
+++ Agreculure/src/app/components/informationimage/informationimage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-informationimage',
+  templateUrl: './informationimage.component.html',
+  styleUrl: './informationimage.component.scss'
+})
+export class InformationimageComponent {
+
+}
